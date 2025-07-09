14eee98ce15c83b6d2ce7a9e1c926505e935b87b
Ridhi chawla <ridhichawla2003@gmail.com>
2025-07-09 14:19:17 +0530

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import AboutNew from './components/aboutNew';
-import Services from './components/Services';
-import Home from './components/home';
+import Services from './components/Service/Servicecard';
+import Home from './pages/Home';
 import Portfolio from './components/Portfolio';
 import Designers from './components/Designers';
-import Pricing from './components/Pricing';
-import Contact from './components/Contact'; 
+import PricingCard from './components/Pricing/PricingCard';
+import AddOnCard from './components/Pricing/AddOnCard';
+import FAQBlock from './components/Pricing/FAQBlock';
+import Contact from './components/Contact';
 import Footer from './components/Footer';
+import Hero from './components/Hero';
+
 function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Hero />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<AboutNew />} />
         <Route path="/services" element={<Services />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/designers" element={<Designers />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/contact" element={<Contact />} /> 
+        <Route path="/pricing" element={<PricingCard />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </>
